e24be0150f1607aa82946c53d67d4510609bbdaf
Alvaro <alvarosimarro_1992@hotmail.com>
2019-03-24 18:54:14 +0100

    fetch and render companies list

--- reto-angular/src/app/pages/mis-negocios/mis-negocios.component.ts
+++ reto-angular/src/app/pages/mis-negocios/mis-negocios.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormService } from 'src/app/service/form.service';
+import { CompanyModel } from 'src/app/model/company.model';
 
 @Component({
   selector: 'mis-negocios',
   templateUrl: 'mis-negocios.component.html',
   styleUrls: ['mis-negocios.component.scss']
 })
-export class NegociosComponent {
 
+export class NegociosComponent {
+  companies: Array<CompanyModel>;
+  constructor(private formService: FormService) {
+
+    this.formService.fetchCompanies().then((companies) => {
+      this.companies = companies
+    })
+  }
 }
+
--- reto-angular/src/app/service/form.service.ts
+++ reto-angular/src/app/service/form.service.ts
@@ -3,34 +3,33 @@ import { HttpClient } from '@angular/common/http';
 import { CompanyModel } from '../model/company.model';
 import { environment } from 'src/environments/environment';
 
-
 const API = environment.api_url;
 
 @Injectable()
 
 export class FormService {
 
-  companies: any = [];
+  companies: Array<CompanyModel>;
 
-  constructor (private httpClient: HttpClient){
-  }
+  constructor (private httpClient: HttpClient){}
 
-  fectchCompany(){
-    return this.httpClient.get(API)
+  fetchCompanies() {
+    return this.httpClient.get(`${API}/company`)
       .toPromise()
-      .then((companies: Array<any>) => {
-        this.companies = companies.map(b => Object.assign({}, b, { publishDate: new Date(b.publishDate) }));
+      .then((companies: Array<CompanyModel>) => {
+        this.companies = companies;
+        return companies;
       });
   }
 
 
-  saveBook(event: CompanyModel){
+  createCompany(event: CompanyModel){
     let observable
     if(event.id){
       observable =  this.httpClient.put(`${API}/${event.id}`, event)
     }else{
       observable = this.httpClient.post(API, event)
     }
-    return observable.toPromise().then(()=> this.fectchCompany);
+    return observable.toPromise().then(()=> this.fetchCompanies);
   }
 }
